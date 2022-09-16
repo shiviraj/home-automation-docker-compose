f3fb4f84e62d32b1d717478ade37ef2fed110c1e
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-09-16 16:21:51 +0530

    Shivi | add start.sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 mkdir -p ~/mosquitto/config ~/mosquitto/data ~/mosquitto/log ~/raspberrypi-db
-docker-compose up -d
+
+if [ $1 == "up" ]; then
+  if [ $2 == "seed" ]; then
+    docker-compose -f docker-compose.yaml -f docker-compose-seed.yaml up -d
+  else
+    docker-compose up -d
+  fi
+else
+    docker-compose -f docker-compose.yaml -f docker-compose-seed.yaml $1
+fi
