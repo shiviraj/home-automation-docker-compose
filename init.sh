fd502732fb0bc9313f2556f5db6af478bb782c6e
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-10-02 13:44:56 +0530

    Shivi | removed unnecessary spaces

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 
 mkdir -p ~/mosquitto/config ~/mosquitto/data ~/mosquitto/log ~/raspberrypi-db
 echo 'persistence true
-      persistence_location /mosquitto/data
-      user mosquitto
-      listener 1883
-      allow_anonymous false
-      log_dest stdout
-      log_dest file /mosquitto/log/mosquitto.log
-      connection_messages true
-      password_file /mosquitto/config/pwfile' >> ~/mosquitto/config/mosquitto.conf
+persistence_location /mosquitto/data
+user mosquitto
+listener 1883
+allow_anonymous false
+log_dest stdout
+log_dest file /mosquitto/log/mosquitto.log
+connection_messages true
+password_file /mosquitto/config/pwfile' >> ~/mosquitto/config/mosquitto.conf
